590ab38c216c7faed32040eff864b1e2c94b1267
Augustindou <augustin.doultremontao@gmail.com>
2022-06-28 03:08:56 +0200

    added induction to easy-testing

--- tests/easy_testing.py
+++ tests/easy_testing.py
@@ -28,7 +28,17 @@ EXAMPLES = {
         r'By the "square mod 3" theorem, we have $q^2 \mod 3 = 1$',
         r"Hence $q^2$ is not divisible by $3$.",
     ],
+    "induction": [
+        r"For any natural number $n$, $n^3 + 2n$ is divisible by $3$.",
+        r"We will use induction on $n$.",
+        r"The base case, $n = 0$, is trivial.",
+        r"For $n = k+1$, the induction hypothesis gives us that $k^3 + 2k$ is divisible by $3$.",
+        r"Therefore, we have a natural number $z$ such that $k^3 + 2k = 3*z$.",
+        r"By expansion, we can derive $(k+1)^3 + 2(k+1) = k^3 + 3k^2 + 5k + 3 = k^3 + 2k + 3(k^2 + k + 1) =3z + 3(k^2 + k + 1) = 3(z+k^2+k+1)$.",
+        r"Hence, $(k+1)^3 + 2(k+1)$ is divisible by $3$, therefore $n^3 + 2n$ is divisible by $3$,",
+    ],
 }
+
 
 if __name__ == "__main__":
     print()
